dba55e37824ab03c8121740033d087d0d6de0b06
LoyaniX <loyanmaksim@gmail.com>
2019-02-06 23:36:35 +0200

    Added test for bubble sort algorithm

---
+++ test/com/loyanix/sort/TestBubbleSort.java
@@ -0,0 +1,27 @@
+package com.loyanix.sort;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class TestBubbleSort {
+
+    @Test
+    public void testSorting(){
+
+        int[] array = new int[10];
+        int[] arrayBase = new int[10];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) ( Math.random() * 100 );
+            arrayBase[i] = array[i];
+        }
+
+        BubbleSort.sort(array);
+        Arrays.sort(arrayBase);
+
+        assert Arrays.equals(array, arrayBase);
+
+    }
+
+}
