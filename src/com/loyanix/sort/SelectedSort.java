4ac622376d17cd5b78110168cbe8b25f23d81e44
LoyaniX <loyanmaksim@gmail.com>
2019-02-06 23:04:38 +0200

    Added implementation of selected sort algorithm

---
+++ src/com/loyanix/sort/SelectedSort.java
@@ -0,0 +1,34 @@
+package com.loyanix.sort;
+
+import java.util.Arrays;
+
+public class SelectedSort {
+
+    public static void main(String[] args) {
+
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) ( Math.random() * 100 );
+        }
+        sort(array);
+    }
+
+    private static void sort(int[] array){
+
+        System.out.println("Default array " + Arrays.toString(array));
+        for (int i = 0; i < array.length; i++){
+            int min = array[i];
+            int index = i;
+            for (int j = i; j < array.length; j++){
+                if (min > array[j]){
+                    min = array[j];
+                    index = j;
+                }
+            }
+            int buffer = array[i];
+            array[i] = min;
+            array[index] = buffer;
+        }
+        System.out.println("Sorted array " + Arrays.toString(array));
+    }
+}
