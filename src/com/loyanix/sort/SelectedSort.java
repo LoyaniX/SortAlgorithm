40ca7d28528576217376aa5ff8221a836fc306c9
LoyaniX <loyanmaksim@gmail.com>
2019-02-06 23:11:47 +0200

    Added test of selected sort algorithm

--- src/com/loyanix/sort/SelectedSort.java
+++ src/com/loyanix/sort/SelectedSort.java
@@ -7,28 +7,36 @@ public class SelectedSort {
     public static void main(String[] args) {
 
         int[] array = new int[10];
+
         for (int i = 0; i < array.length; i++) {
             array[i] = (int) ( Math.random() * 100 );
         }
+
         sort(array);
     }
 
-    private static void sort(int[] array){
+    public static void sort(int[] array){
 
         System.out.println("Default array " + Arrays.toString(array));
+
         for (int i = 0; i < array.length; i++){
+
             int min = array[i];
             int index = i;
+
             for (int j = i; j < array.length; j++){
                 if (min > array[j]){
                     min = array[j];
                     index = j;
                 }
             }
+
             int buffer = array[i];
+
             array[i] = min;
             array[index] = buffer;
         }
+
         System.out.println("Sorted array " + Arrays.toString(array));
     }
 }
--- test/com/loyanix/sort/TestSort.java
+++
@@ -1,4 +0,0 @@
-package com.loyanix.sort;
-
-public class TestSort {
-}
