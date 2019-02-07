f7b89e985da5eb84fd27e5542dc9abd3d3049da3
Maksim Loyan <loyanmaksim@gmail.com>
2019-02-07 13:11:47 +0200

    Refactor

--- src/com/loyanix/sort/QuickSort.java
+++ src/com/loyanix/sort/QuickSort.java
@@ -7,40 +7,58 @@ public class QuickSort {
     private static int[] poinArray;
 
     public static void main(String[] args) {
+
         int[]  array = new int[13];
+
         for (int i = 0; i < array.length; i++) {
             array[i] = (int) ( Math.random() * 100 );
         }
 
         System.out.println("Default array " + Arrays.toString(array));
+
         sort(array);
+
         System.out.println("Sorted array " + Arrays.toString(array));
     }
 
     public static void sort(int[] values) {
 
         poinArray = values;
+
         quicksort(0, poinArray.length - 1);
     }
 
     private static void quicksort(int startArray, int endArray) {
+
         int i = startArray;
         int j = endArray;
         int pivot = poinArray[startArray + (endArray - startArray) / 2];
 
         while (i <= j) {
 
-            while (poinArray[i] < pivot) i++;
-            while (poinArray[j] > pivot) j--;
+            while (poinArray[i] < pivot){
+                i++;
+            }
+            while (poinArray[j] > pivot){
+                j--;
+            }
             if (i <= j) {
+
                 int temp = poinArray[i];
+
                 poinArray[i] = poinArray[j];
                 poinArray[j] = temp;
+
                 i++;
                 j--;
             }
         }
-        if (startArray < j) quicksort(startArray, j);
-        if (i < endArray) quicksort(i, endArray);
+
+        if (startArray < j){
+            quicksort(startArray, j);
+        }
+        if (i < endArray){
+            quicksort(i, endArray);
+        }
     }
 }
